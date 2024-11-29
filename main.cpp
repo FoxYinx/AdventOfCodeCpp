79d4a30f5918288a0b161f96f04b3462fa5e7b4c
joshuadwightred <tollemerkevin@gmail.com>
2024-11-29 17:06:44 +0100

    Year 2022 - Day 1

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ifstream f("ressources/day1.txt");
+
+    if (!f.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+    cout << "File successfully opened!" << endl;
+
+    string s;
+    int maxCalories = 0;
+    int currentCalories = 0;
+    while(getline(f, s)) {
+        if (s.empty()) {
+            if (currentCalories > maxCalories) maxCalories = currentCalories;
+            currentCalories = 0;
+        } else {
+            currentCalories += stoi(s);
+        }
+    }
+
+    cout << maxCalories << endl;
+
+    f.close();
+    return 0;
+}
