d32ec2b75be785800a44940a4d603e6b489a1e18
joshuadwightred <tollemerkevin@gmail.com>
2024-11-30 15:26:31 +0100

    Year 2022 - Day 3 - Puzzle 1

--- Year2022/day3/puzzle1.cpp
+++ Year2022/day3/puzzle1.cpp
@@ -1,3 +1,37 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
 int year2022_day3_puzzle1() {
+    ifstream f("ressources/day3.txt");
+
+    if (!f.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+    cout << "File successfully opened!" << endl;
+
+    string s;
+    int totalPriority = 0;
+    while (getline(f, s)) {
+        char toFind = 'A';
+        for (int i = 0; i < s.length() / 2; i++) {
+            bool flag = false;
+            for (int j = s.length() / 2; j < s.length(); j++) {
+                if (s[i] == s[j]) {
+                    flag = true;
+                    toFind = s[i];
+                    break;
+                }
+            }
+            if (flag) break;
+        }
+        int number = toFind - 96;
+        if (number < 0) number += 58;
+        totalPriority += number;
+    }
+
+    cout << totalPriority << endl;
     return 0;
 }
--- Year2022/day3/puzzle2.cpp
+++ Year2022/day3/puzzle2.cpp
@@ -1,3 +1,21 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
 int year2022_day3_puzzle2() {
+    ifstream f("ressources/day3.txt");
+
+    if (!f.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+    cout << "File successfully opened!" << endl;
+
+    string s;
+    while (getline(f, s)) {
+
+    }
+
     return 0;
 }
