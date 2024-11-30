36aa2e82f96a0fbac599eaf506eaf2c73ac986b4
joshuadwightred <tollemerkevin@gmail.com>
2024-11-30 17:52:31 +0100

    Year 2022 - Day 3 - Puzzle 2

--- Year2022/day3/puzzle2.cpp
+++ Year2022/day3/puzzle2.cpp
@@ -12,10 +12,36 @@ int year2022_day3_puzzle2() {
     }
     cout << "File successfully opened!" << endl;
 
-    string s;
-    while (getline(f, s)) {
-
+    string first_line;
+    string second_line;
+    string third_line;
+    int totalPriority = 0;
+    while (f.peek() != EOF) {
+        getline(f, first_line);
+        getline(f, second_line);
+        getline(f, third_line);
+        char toFind = 'A';
+        for (int i = 0; i < first_line.length(); i++) {
+            bool flag = false;
+            for (int j = 0; j < second_line.length(); j++) {
+                if (first_line[i] == second_line[j]) {
+                    for (int k = 0; k < third_line.length(); k++) {
+                        if (second_line[j] == third_line[k]) {
+                            flag = true;
+                            toFind = first_line[i];
+                            break;
+                        }
+                    }
+                }
+                if (flag) break;
+            }
+            if (flag) break;
+        }
+        int number = toFind - 96;
+        if (number < 0) number += 58;
+        totalPriority += number;
     }
 
+    cout << totalPriority << endl;
     return 0;
 }
