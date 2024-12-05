e41b94d942dc0ac331d343d4cdeea84350ed6d12
yinx <tollemerkevin@gmail.com>
2024-12-05 19:58:55 +0100

    Year 2019 - Day 5

--- Year2019/day5/puzzle2.cpp
+++ Year2019/day5/puzzle2.cpp
@@ -1,3 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include "../intcode.h"
+
+using namespace std;
+
 int year2019_day5_puzzle2() {
+    ifstream f("ressources/Year2019/day5.txt");
+
+    if (!f.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+    cout << "File successfully opened!" << endl;
+
+    string s;
+    string tmp;
+    vector<int> program;
+    while (getline(f, s)) {
+        stringstream ss(s);
+        while (getline(ss, tmp, ',')) {
+            program.push_back(stoi(tmp));
+        }
+    }
+
+    Intcode intcode;
+    while (true) {
+        int instruction = program[intcode.pointer];
+        if (instruction == 99) break;
+        intcode.processInstruction(instruction, program);
+        if (intcode.stuck) return 1;
+    }
+
     return 0;
 }
--- Year2019/intcode.cpp
+++ Year2019/intcode.cpp
@@ -11,13 +11,31 @@ Intcode::Intcode() {
 }
 
 void Intcode::processInstruction(int instruction, vector<int>& program) {
-    switch (instruction) {
+
+    int realInstruction = instruction % 100;
+
+    int arg1;
+    int arg2;
+
+    switch (realInstruction) {
         case 1:
-            program[program[pointer + 3]] = program[program[pointer + 1]] + program[program[pointer + 2]];
+            arg1 = ((instruction / 100) % 10 == 1) ? program[pointer + 1] : program[program[pointer + 1]];
+            arg2 = ((instruction / 1000) % 10 == 1) ? program[pointer + 2] : program[program[pointer + 2]];
+            if ((instruction / 10000) % 10 == 1) {
+                program[pointer + 3] = arg1 + arg2;
+            } else {
+                program[program[pointer + 3]] = arg1 + arg2;
+            }
             this->pointer += 4;
             break;
         case 2:
-            program[program[pointer + 3]] = program[program[pointer + 1]] * program[program[pointer + 2]];
+            arg1 = ((instruction / 100) % 10 == 1) ? program[pointer + 1] : program[program[pointer + 1]];
+            arg2 = ((instruction / 1000) % 10 == 1) ? program[pointer + 2] : program[program[pointer + 2]];
+            if ((instruction / 10000) % 10 == 1) {
+                program[pointer + 3] = arg1 + arg2;
+            } else {
+                program[program[pointer + 3]] = arg1 * arg2;
+            }
             this->pointer += 4;
             break;
         case 3:
@@ -26,8 +44,50 @@ void Intcode::processInstruction(int instruction, vector<int>& program) {
             this->pointer += 2;
             break;
         case 4:
-            cout << "output: " << program[program[pointer + 1]] << endl;
+            if ((instruction / 100) % 10 == 1) {
+                cout << "output: " << program[pointer + 1] << endl;
+            } else {
+                cout << "output: " << program[program[pointer + 1]] << endl;
+            }
             this->pointer += 2;
+            break;
+        case 5:
+            arg1 = ((instruction / 100) % 10 == 1) ? program[pointer + 1] : program[program[pointer + 1]];
+            arg2 = ((instruction / 1000) % 10 == 1) ? program[pointer + 2] : program[program[pointer + 2]];
+            if (arg1) {
+                this->pointer = arg2;
+            } else {
+                this->pointer += 3;
+            }
+            break;
+        case 6:
+            arg1 = ((instruction / 100) % 10 == 1) ? program[pointer + 1] : program[program[pointer + 1]];
+            arg2 = ((instruction / 1000) % 10 == 1) ? program[pointer + 2] : program[program[pointer + 2]];
+            if (!arg1) {
+                this->pointer = arg2;
+            } else {
+                this->pointer += 3;
+            }
+            break;
+        case 7:
+            arg1 = ((instruction / 100) % 10 == 1) ? program[pointer + 1] : program[program[pointer + 1]];
+            arg2 = ((instruction / 1000) % 10 == 1) ? program[pointer + 2] : program[program[pointer + 2]];
+            if ((instruction / 10000) % 10 == 1) {
+                program[pointer + 3] = (arg1 < arg2) ? 1 : 0;
+            } else {
+                program[program[pointer + 3]] = (arg1 < arg2) ? 1 : 0;
+            }
+            this->pointer += 4;
+            break;
+        case 8:
+            arg1 = ((instruction / 100) % 10 == 1) ? program[pointer + 1] : program[program[pointer + 1]];
+            arg2 = ((instruction / 1000) % 10 == 1) ? program[pointer + 2] : program[program[pointer + 2]];
+            if ((instruction / 10000) % 10 == 1) {
+                program[pointer + 3] = (arg1 == arg2) ? 1 : 0;
+            } else {
+                program[program[pointer + 3]] = (arg1 == arg2) ? 1 : 0;
+            }
+            this->pointer += 4;
             break;
         default:
             cerr << "Unknown opcode: " << instruction << endl;
