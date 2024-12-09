7f866d7fd709834ae4d53a793eabdea3656c3a29
joshuadwightred <tollemerkevin@gmail.com>
2024-12-09 17:32:55 +0100

    Year 2024 - Day 9 - Puzzle 2

--- Year2024/day9/puzzle2.cpp
+++ Year2024/day9/puzzle2.cpp
@@ -1,11 +1,12 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <array>
 
 using namespace std;
 
 int year2024_day9_puzzle2() {
-    ifstream f("ressources/Year2024/test.txt");
+    ifstream f("ressources/Year2024/day9.txt");
 
     if (!f.is_open()) {
         cerr << "Error opening file" << endl;
@@ -30,41 +31,47 @@ int year2024_day9_puzzle2() {
         }
     }
 
-    unsigned long fin = disk.size() - 1;
-    for (int j = 0; j < disk.size(); j++) {
-        int size = 0;
-        do {
-            size++;
-            fin--;
-        } while (disk[fin] == disk[fin + 1]);
+    vector<array<int, 2>> empty;
+    vector<array<int, 2>> block;
 
-        int emptySize = 0;
-        while (disk[j] == -1) {
-            emptySize++;
-            j++;
-        }
-        if (emptySize >= size) {
-            for (int i = 0; i < size; i++) {
-                disk[j - i] = disk[fin];
-                disk[fin - 1] = -1;
-            }
+    for (int i = 0; i < disk.size(); i++) {
+        if (disk.at(i) != -1) {
+            block.push_back({i, 1});
             do {
-                fin--;
-            } while (disk[fin] == -1);
+                i++;
+                block.at(block.size() - 1)[1]++;
+                if (i == disk.size()) break;
+            } while (disk.at(i - 1) == disk.at(i));
+            block.at(block.size() - 1)[1]--;
+            i--;
         } else {
-            //fixme: que faire quand le premier gap n'est pas bon
+            empty.push_back({i, 1});
+            do {
+                i++;
+                empty.at(empty.size() - 1)[1]++;
+                if (i == disk.size()) break;
+            } while (disk.at(i - 1) == disk.at(i));
+            empty.at(empty.size() - 1)[1]--;
+            i--;
         }
-
-        if (j == fin) break;
     }
 
-    for (auto c : disk) {
-        if (c == -1) cout << '.';
-        else cout << c;
+    for (int i = block.size() - 1; i >= 0; i--) {
+        for (auto & space : empty) {
+            if (block[i][0] <= space[0]) break;
+            if (space[1] >= block[i][1] ) {
+                for (int j = 0; j < block[i][1]; j++) {
+                    disk[space[0]] = disk[block[i][0]];
+                    space[0]++;
+                    space[1]--;
+                }
+                for (int j = 0; j < block[i][1]; j++) disk[block[i][0] + j] = -1;
+                break;
+            }
+        }
     }
-    cout << endl;
 
-    unsigned long checksum = 0;
+    long long checksum = 0;
     for (int i = 0; i < disk.size(); i++) {
         if (disk[i] != -1) checksum += i * disk[i];
     }
