3149c845edd8a854188a2495a18d74e228daec91
yinx <tollemerkevin@gmail.com>
2024-12-10 10:03:41 +0100

    Year 2024 - Day 10 - Puzzle 1 (ongoing)

--- Year2024/day10/puzzle1.cpp
+++ Year2024/day10/puzzle1.cpp
@@ -1,5 +1,9 @@
 #include <fstream>
 #include <iostream>
+#include <array>
+#include <vector>
+
+#define SIZE 8
 
 using namespace std;
 
@@ -13,8 +17,23 @@ int year2024_day10_puzzle1() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    array<array<int, SIZE>, SIZE> map = {};
+    int line = 0;
     while (getline(f, s)) {
+        for (int i = 0; i < SIZE; i++) {
+            map[line][i] = s.at(i) - '0';
+        }
+        line++;
+    }
 
+    int trails = 0;
+    vector<array<int, 2>> tmp;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (map[i][j] == 0) {
+                if ()
+            }
+        }
     }
 
     return 0;
