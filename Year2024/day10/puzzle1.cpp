fe62c8752f2c8b7fdd8b049a68d4aaa2252015df
joshuadwightred <tollemerkevin@gmail.com>
2024-12-10 14:43:14 +0100

    Year 2024 - Day 10

--- Year2024/day10/puzzle1.cpp
+++ Year2024/day10/puzzle1.cpp
@@ -1,14 +1,17 @@
 #include <fstream>
 #include <iostream>
 #include <array>
-#include <vector>
+#include <queue>
+#include <set>
 
-#define SIZE 8
+#define SIZE 43
 
 using namespace std;
 
+void searchUnique(const array<array<int, SIZE>, SIZE>& map, int i, int j, set<array<int, 4>>& trails);
+
 int year2024_day10_puzzle1() {
-    ifstream f("ressources/Year2024/test.txt");
+    ifstream f("ressources/Year2024/day10.txt");
 
     if (!f.is_open()) {
         cerr << "Error opening file" << endl;
@@ -26,15 +29,43 @@ int year2024_day10_puzzle1() {
         line++;
     }
 
-    int trails = 0;
-    vector<array<int, 2>> tmp;
+    set<array<int, 4>> trails;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (map[i][j] == 0) {
-                if ()
-            }
+            if (map[i][j] == 0) searchUnique(map, i, j, trails);
         }
     }
 
+    cout << trails.size() << endl;
     return 0;
+}
+
+void searchUnique(const array<array<int, SIZE>, SIZE>& map, const int i, const int j, set<array<int, 4>>& trails) {
+    queue<array<int, 2>> file;
+    file.push({i, j});
+
+    while (!file.empty()) {
+        array<int, 2> node = file.front();
+        const int y = node[0];
+        const int x = node[1];
+        file.pop();
+
+        if (y - 1 >= 0 && map[y - 1][x] == map[y][x] + 1) {
+            if (map[y - 1][x] == 9) trails.insert({i, j, y - 1, x});
+            else file.push({y - 1, x});
+        }
+        if (y + 1 < SIZE && map[y + 1][x] == map[y][x] + 1) {
+            if (map[y + 1][x] == 9) trails.insert({i, j, y + 1, x});
+            else file.push({y + 1, x});
+        }
+
+        if (x - 1 >= 0 && map[y][x - 1] == map[y][x] + 1) {
+            if (map[y][x - 1] == 9) trails.insert({i, j, y, x - 1});
+            else file.push({y, x - 1});
+        }
+        if (x + 1 < SIZE && map[y][x + 1] == map[y][x] + 1) {
+            if (map[y][x + 1] == 9) trails.insert({i, j, y, x + 1});
+            else file.push({y, x + 1});
+        }
+    }
 }
--- Year2024/day10/puzzle2.cpp
+++ Year2024/day10/puzzle2.cpp
@@ -1,10 +1,16 @@
 #include <fstream>
 #include <iostream>
+#include <array>
+#include <queue>
+
+#define SIZE 43
 
 using namespace std;
 
+void search(const array<array<int, SIZE>, SIZE>& map, int i, int j, int& trails);
+
 int year2024_day10_puzzle2() {
-    ifstream f("ressources/Year2024/test.txt");
+    ifstream f("ressources/Year2024/day10.txt");
 
     if (!f.is_open()) {
         cerr << "Error opening file" << endl;
@@ -13,9 +19,52 @@ int year2024_day10_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    array<array<int, SIZE>, SIZE> map = {};
+    int line = 0;
     while (getline(f, s)) {
-
+        for (int i = 0; i < SIZE; i++) {
+            map[line][i] = s.at(i) - '0';
+        }
+        line++;
     }
 
+    int trails;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (map[i][j] == 0) search(map, i, j, trails);
+        }
+    }
+
+    cout << trails << endl;
     return 0;
+}
+
+void search(const array<array<int, SIZE>, SIZE>& map, const int i, const int j, int& trails) {
+    queue<array<int, 2>> file;
+    file.push({i, j});
+
+    while (!file.empty()) {
+        array<int, 2> node = file.front();
+        const int y = node[0];
+        const int x = node[1];
+        file.pop();
+
+        if (y - 1 >= 0 && map[y - 1][x] == map[y][x] + 1) {
+            if (map[y - 1][x] == 9) trails++;
+            else file.push({y - 1, x});
+        }
+        if (y + 1 < SIZE && map[y + 1][x] == map[y][x] + 1) {
+            if (map[y + 1][x] == 9) trails++;
+            else file.push({y + 1, x});
+        }
+
+        if (x - 1 >= 0 && map[y][x - 1] == map[y][x] + 1) {
+            if (map[y][x - 1] == 9) trails++;
+            else file.push({y, x - 1});
+        }
+        if (x + 1 < SIZE && map[y][x + 1] == map[y][x] + 1) {
+            if (map[y][x + 1] == 9) trails++;
+            else file.push({y, x + 1});
+        }
+    }
 }
