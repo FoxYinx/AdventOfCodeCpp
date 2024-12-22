bb293a3a752d24aa1301f4af31e6880c9a02cc77
joshuadwightred <tollemerkevin@gmail.com>
2024-12-22 21:13:17 +0100

    Year 2024 - Day 22 - Puzzle 1

--- Year2024/day22/puzzle1.cpp
+++ Year2024/day22/puzzle1.cpp
@@ -1,7 +1,10 @@
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
+
+uint64_t deriveSecret(unsigned long long secret);
 
 int year2024_day22_puzzle1() {
     ifstream f("ressources/Year2024/day22.txt");
@@ -13,9 +16,28 @@ int year2024_day22_puzzle1() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    uint64_t sum = 0;
     while (getline(f, s)) {
-
+        uint64_t initialValue = stoull(s);
+        for (int i = 0; i < 2000; i++) {
+            initialValue = deriveSecret(initialValue);
+        }
+        sum += initialValue;
     }
 
+    cout << sum << endl;
     return 0;
+}
+
+uint64_t deriveSecret(unsigned long long secret) {
+    const uint64_t mul64 = secret << 6;
+    secret ^= mul64;
+    secret %= 16777216;
+    const uint64_t div32 = secret >> 5;
+    secret ^= div32;
+    secret %= 16777216;
+    const uint64_t game = secret << 11;
+    secret ^= game;
+    secret %= 16777216;
+    return secret;
 }
