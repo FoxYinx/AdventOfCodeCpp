7ea0035014260b6777b8862b47e951d81c9ffe18
joshuadwightred <tollemerkevin@gmail.com>
2024-12-23 15:56:00 +0100

    Year 2024 - Day 23 - Puzzle 2 (ongoing)

--- Year2024/day23/puzzle2.cpp
+++ Year2024/day23/puzzle2.cpp
@@ -1,10 +1,14 @@
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <regex>
+#include <vector>
+#include "utils.h"
 
 using namespace std;
 
 int year2024_day23_puzzle2() {
-    ifstream f("ressources/Year2024/day23.txt");
+    ifstream f("ressources/Year2024/test.txt");
 
     if (!f.is_open()) {
         cerr << "Error opening file" << endl;
@@ -13,9 +17,28 @@ int year2024_day23_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    const regex regexp(R"((\w+)-(\w+))");
+    smatch sm;
+    map<string, vector<string>> links = {};
     while (getline(f, s)) {
+        while (regex_search(s, sm, regexp)) {
+            links[sm[1]].push_back(sm[2]);
+            links[sm[2]].push_back(sm[1]);
+            s = sm.suffix();
+        }
+    }
+    string password;
 
+    for (const auto &[key, value] : links) {
+        for (int i = 0; i < value.size() - 1; i++) {
+            if (value[i] > key) {
+                for (int j = 0; j < value.size(); j++) {
+                    if (links[value[i]][j] > value[i] && contains(links[key], links[value[i]][j])) password = key + "," + value[i] + "," + links[value[i]][j];
+                }
+            }
+        }
     }
 
+    cout << password << endl;
     return 0;
 }
