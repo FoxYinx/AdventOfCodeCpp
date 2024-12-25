e887fe3e253519fc3e2c1817970e428180ccca24
joshuadwightred <tollemerkevin@gmail.com>
2024-12-25 20:53:15 +0100

    Year 2024 - Day 25 - Puzzle 2

--- Year2024/day25/puzzle2.cpp
+++ Year2024/day25/puzzle2.cpp
@@ -1,21 +1,8 @@
-#include <fstream>
 #include <iostream>
 
 using namespace std;
 
 int year2024_day25_puzzle2() {
-    ifstream f("ressources/Year2024/day25.txt");
-
-    if (!f.is_open()) {
-        cerr << "Error opening file" << endl;
-        return 1;
-    }
-    cout << "File successfully opened!" << endl;
-
-    string s;
-    while (getline(f, s)) {
-
-    }
-
+    cout << "Merry Christmas!" << endl;
     return 0;
 }
